c0e2c91e4cb8ca0df563c986fd6ff8d7e0b46f2e
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-03-17 14:35:28 -0300

    Added illuminate plugin

--- lua/core/plugins.lua
+++ lua/core/plugins.lua
@@ -53,5 +53,9 @@ require("lazy").setup({
         "nvim-telescope/telescope.nvim",
         dependencies = { "nvim-treesitter/nvim-treesitter" }
     },
-    { 'nvim-telescope/telescope-fzf-native.nvim', build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build' },
+    {
+        'nvim-telescope/telescope-fzf-native.nvim',
+        build = 'cmake -S. -Bbuild -DCMAKE_BUILD_TYPE=Release && cmake --build build --config Release && cmake --install build --prefix build'
+    },
+    "RRethy/vim-illuminate",
 })
