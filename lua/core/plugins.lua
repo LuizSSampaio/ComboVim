a74b259822e072a8d9d2d5d7765f2ebcc1cdb42f
Luiz Henrique Silva Sampaio <luizhsampaio07@gmail.com>
2024-03-16 21:29:06 -0300

    Added plugins list

---
+++ lua/core/plugins.lua
@@ -0,0 +1,38 @@
+require("lazy").setup({
+    { "catppuccin/nvim", name = "catppuccin", priority = 1000 },
+    "nvim-tree/nvim-tree.lua",
+    "nvim-tree/nvim-web-devicons",
+    "nvim-lualine/lualine.nvim",
+    "nvim-treesitter/nvim-treesitter",
+    { 
+        "nvim-neotest/neotest",
+        dependencies = {
+            "nvim-lua/plenary.nvim",
+            "antoinemadec/FixCursorHold.nvim",
+            "nvim-treesitter/nvim-treesitter"
+        }
+    },
+    "lewis6991/gitsigns.nvim",
+    {
+        "hrsh7th/nvim-cmp",
+        dependencies = {
+            {
+                "L3MON4D3/LuaSnip",
+                dependencies = "rafamadriz/friendly-snippets"
+            },
+            "windwp/nvim-autopairs",
+            "saadparwaiz1/cmp_luasnip",
+            "hrsh7th/cmp-nvim-lua",
+            "hrsh7th/cmp-nvim-lsp",
+            "hrsh7th/cmp-buffer",
+            "hrsh7th/cmp-path"
+        }
+    },
+    "williamboman/mason.nvim",
+    "williamboman/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+    {
+        "nvim-telescope/telescope.nvim",
+        dependencies = { "nvim-treesitter/nvim-treesitter" }
+    }
+})
